583dc70a121a7571a4b67146a5d24bdab4104f89
vasc <nico@vasc.dev>
2026-08-31 01:12:59 +0200

    feat: spinner animation for export btn

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,19 +1,27 @@
-import { Download } from "lucide-react";
+import { Download, Loader2 } from "lucide-react";
 import { useSelectedMemories } from "../lib/useSelectedMemories.ts";
 import { generateMemoriesFromZip } from "../lib/bereal.ts";
+import { useState } from "react";
 
 export default function ExportButton({ zipFile }: { zipFile: File }) {
+  const [processing, setProcessing] = useState(false);
   const memoryIds = useSelectedMemories((state) => state.memoryIds);
 
   return (
     <button
-      className="flex items-center justify-center bg-white px-4 py-2 rounded-md text-black disabled:bg-white/40"
-      disabled={memoryIds.length <= 0}
+      className="flex items-center gap-1 justify-center bg-white px-4 py-2 rounded-md text-black disabled:bg-white/40"
+      disabled={memoryIds.length <= 0 || processing}
       onClick={async () => {
+        setProcessing(true);
         await generateMemoriesFromZip(zipFile, memoryIds);
+        setProcessing(false);
       }}
     >
-      <Download className="mr-1 h-4 w-4" />
+      {processing ? (
+        <Loader2 className="size-4 animate-spin" />
+      ) : (
+        <Download className="size-4" />
+      )}
       <span>Export {memoryIds.length}</span>
     </button>
   );
